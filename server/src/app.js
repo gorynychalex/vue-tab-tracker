8d2a909074c7e930497244722daea333004844bc
gorynychalex <gorynychalex@gmail.com>
2017-11-05 05:12:14 +1000

    app.js modify

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,24 @@
 console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/hello', (req, res) => {
+  res.send({
+    'message': 'hello world!'
+  })
+})
+
+app.get('/status', (req, res) => {
+  res.send({
+    'message': 'status'
+  })
+})
+
+app.listen(process.env.PORT || 8082)
